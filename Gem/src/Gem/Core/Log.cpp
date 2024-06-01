86b16422593d25c2aa2b003e508d7247ab953f33
Jinkun <Jinkun@qq.com>
2024-06-01 16:00:31 +0800

    Log messages are now additionally sent to Gem.log file

--- Gem/src/Gem/Core/Log.cpp
+++ Gem/src/Gem/Core/Log.cpp
@@ -1,6 +1,7 @@
 #include "gempch.h"
 #include "Log.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
+#include <spdlog/sinks/basic_file_sink.h>
 
 namespace Gem
 {
@@ -9,11 +10,24 @@ namespace Gem
 
 	void Log::Init()
 	{
-		spdlog::set_pattern("%^[%T] %n: %v%$");
-		s_CoreLogger = spdlog::stdout_color_mt("GEM");
-		s_CoreLogger->set_level(spdlog::level::trace);
+		// 现在将日志添加到文件中
 
-		s_ClientLogger = spdlog::stdout_color_mt("APP");
+		std::vector<spdlog::sink_ptr> logSinks;
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::stdout_color_sink_mt>());
+		logSinks.emplace_back(std::make_shared<spdlog::sinks::basic_file_sink_mt>("Gem.log", true));
+
+		logSinks[0]->set_pattern("%^[%T] %n: %v%$");
+		logSinks[1]->set_pattern("[%T] [%l] %n: %v");
+
+		s_CoreLogger = CreateRef<spdlog::logger>("GEM", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_CoreLogger);
+
+		s_CoreLogger->set_level(spdlog::level::trace);
+		s_CoreLogger->flush_on(spdlog::level::trace);
+
+		s_ClientLogger = CreateRef<spdlog::logger>("APP", begin(logSinks), end(logSinks));
+		spdlog::register_logger(s_ClientLogger);
 		s_ClientLogger->set_level(spdlog::level::trace);
+		s_ClientLogger->flush_on(spdlog::level::trace);
 	}
 }
